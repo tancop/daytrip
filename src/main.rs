c4fde2f3b0e0a06ee8266a486bbc165e36ebb9e7
Teodor Potancok <teodor.potancok@gmail.com>
2025-05-31 09:26:29 +0200

    Use clap for command line args

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,42 @@
+use clap::{Parser, command};
 use download::Loader;
 use librespot::core::{
     Session, SessionConfig, SpotifyId, cache::Cache, error::ErrorKind, spotify_id::SpotifyItemType,
 };
 use regex::Regex;
+use serde::Serialize;
 
 mod auth;
 mod download;
+
+#[derive(clap::ValueEnum, Clone, Default, Debug, Serialize)]
+#[serde(rename_all = "kebab-case")]
+enum OutputFormat {
+    #[default]
+    Opus,
+    Wav,
+    Ogg,
+    Mp3,
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Share link or Spotify URI for the downloaded item
+    url: String,
+
+    /// Output audio format
+    #[arg(short, long, value_enum, default_value_t)]
+    format: OutputFormat,
+
+    /// Remove tags like `(feat. Artist Name)` from track titles
+    #[arg(short, long, default_value_t = false)]
+    remove_feature_tags: bool,
+
+    /// Add track number to file names when downloading an album or playlist
+    #[arg(short, long, default_value_t = false)]
+    number_tracks: bool,
+}
 
 fn parse_item_type(item_type: &str) -> SpotifyItemType {
     match item_type {
@@ -24,7 +55,7 @@ fn parse_item_type(item_type: &str) -> SpotifyItemType {
 #[tokio::main]
 async fn main() {
     env_logger::init();
-    let args: Vec<_> = std::env::args().collect();
+    let args = Args::parse();
 
     let cache = Cache::new(
         Some("./daytrip-cache"),
@@ -55,15 +86,15 @@ async fn main() {
         }
     };
 
-    let item_ref = if (&args[1]).starts_with("spotify:") {
-        let Ok(item_ref) = SpotifyId::from_base62(&args[1]) else {
-            log::error!("Invalid Spotify ID: {}", &args[1]);
+    let item_ref = if args.url.starts_with("spotify:") {
+        let Ok(item_ref) = SpotifyId::from_base62(&args.url) else {
+            log::error!("Invalid Spotify ID: {}", &args.url);
             std::process::exit(1);
         };
         item_ref
     } else {
         let re = Regex::new(r"spotify\.com/(\w+)/(\w+)").unwrap();
-        if let Some(res) = re.captures(&args[1]) {
+        if let Some(res) = re.captures(&args.url) {
             let item_type = &res[1];
             let id = &res[2];
             let Ok(mut item_ref) = SpotifyId::from_base62(id) else {
@@ -73,8 +104,8 @@ async fn main() {
             item_ref.item_type = parse_item_type(item_type);
             item_ref
         } else {
-            let Ok(mut item_ref) = SpotifyId::from_base62(&args[1]) else {
-                log::error!("Invalid Spotify ID: {}", &args[1]);
+            let Ok(mut item_ref) = SpotifyId::from_base62(&args.url) else {
+                log::error!("Invalid Spotify ID: {}", &args.url);
                 std::process::exit(1);
             };
             item_ref.item_type = SpotifyItemType::Track;
